a19df48adb532ce25508edc720a8056f72d975d1
Petr Messner <petr.messner@gmail.com>
2018-07-29 11:06:36 +0200

    wip homework submission - comments UI

--- frontend/components/HomeworkSolutionForm.js
+++ frontend/components/HomeworkSolutionForm.js
@@ -2,29 +2,47 @@ import React from 'react'
 import { Button } from 'semantic-ui-react'
 import CodeEditor from './CodeEditor'
 
+const defaultCode = '# sem napiš svoje řešení\n'
+
 export default class HomeworkSubmission extends React.Component {
 
-  state = {
-    code: '# sem napiš svoje řešení\n',
+  constructor(props) {
+    super(props)
+    this.state = {
+      code: props.code || defaultCode
+    }
   }
 
   handleCodeChange = (newValue) => {
-    this.setState({ code: newValue })
+    if (!this.props.loading) {
+      this.setState({ code: newValue })
+    }
+  }
+
+  handleSubmitClick = () => {
+    const { code } = this.state
+    this.props.onSubmit({ code })
   }
 
   render() {
+    const { loading } = this.props
     const { code } = this.state
     return (
       <div className='HomeworkSubmitForm'>
         <CodeEditor
           value={code}
           onValueChange={this.handleCodeChange}
+          disabled={loading}
         />
         <div style={{ marginTop: 10 }}>
           <Button
             size='small'
             primary
-            content='Odevzdat'
+            icon='send'
+            content='Odeslat'
+            onClick={this.handleSubmitClick}
+            loading={loading}
+            disabled={loading}
           />
         </div>
       </div>
--- frontend/components/HomeworkSubmission.js
+++ frontend/components/HomeworkSubmission.js
@@ -1,20 +1,127 @@
 import React from 'react'
 import { Button } from 'semantic-ui-react'
-import HomeworkSubmitForm from './HomeworkSubmitForm'
+import { Comment, Form, Header } from 'semantic-ui-react'
+import HomeworkSolutionForm from './HomeworkSolutionForm'
+
+const HomeworkSolution = ({ code }) => (
+  <div className='HomeworkSolution'>
+    <h4>Odevzdané řešení</h4>
+    <pre>{code}</pre>
+  </div>
+)
+
+class CommentReply extends React.Component {
+
+  render() {
+    return (
+      <Comment>
+        <Comment.Avatar src='https://react.semantic-ui.com/images/avatar/small/jenny.jpg' />
+        <Comment.Content>
+          <Comment.Author as='a'>Jenny Hess</Comment.Author>
+          <Comment.Metadata>
+            <div>Just now</div>
+          </Comment.Metadata>
+          <Comment.Text>Elliot you are always so right :)</Comment.Text>
+          <Comment.Actions>
+            <Comment.Action>Reply</Comment.Action>
+          </Comment.Actions>
+        </Comment.Content>
+      </Comment>
+    )
+  }
+
+}
+
+
+class CommentThread extends React.Component {
+
+  render() {
+    return (
+      <Comment>
+        <Comment.Avatar src='https://react.semantic-ui.com/images/avatar/small/elliot.jpg' />
+        <Comment.Content>
+          <Comment.Author as='a'>Elliot Fu</Comment.Author>
+          <Comment.Metadata>
+            <div>Yesterday at 12:30AM</div>
+          </Comment.Metadata>
+          <Comment.Text>
+            <p>This has been very useful for my research. Thanks as well!</p>
+          </Comment.Text>
+          <Comment.Actions>
+            <Comment.Action>Reply</Comment.Action>
+          </Comment.Actions>
+        </Comment.Content>
+        <Comment.Group>
+          <CommentReply />
+          <CommentReply />
+        </Comment.Group>
+      </Comment>
+
+    )
+  }
+
+}
+
+class HomeworkComments extends React.Component {
+
+  state = {
+    replyTo: null,
+  }
+
+  render() {
+    const { replyTo } = this.state
+    return (
+      <div style={{ marginTop: '1.5rem' }}>
+        <Comment.Group>
+          <CommentThread />
+
+          {replyTo !== null && (
+            <Form reply>
+              <Form.TextArea />
+              <Button content='Add Reply' size='small' icon='edit' primary />
+            </Form>
+          )}
+        </Comment.Group>
+      </div>
+    )
+  }
+}
 
 export default class HomeworkSubmission extends React.Component {
 
   state = {
     //open: false,
     open: true,
+    //currentSolution: null,
+    currentSolution: { code: 'foo\nbar\n' },
+    editCurrentSolution: false,
+    submitInProgress: false,
   }
 
   handleOpenButton = () => {
     this.setState({ open: true })
   }
 
+  handleSubmitSolution = async ({ code }) => {
+    this.setState({
+      submitInProgress: true,
+    })
+    return
+    this.setState({
+      submitInProgress: false,
+      currentSolution: { code },
+      editCurrentSolution: false,
+    })
+  }
+
+  handleEditButton = async () => {
+    this.setState({
+      editCurrentSolution: true,
+    })
+  }
+
   render() {
-    const { open } = this.state
+    const { open, currentSolution, editCurrentSolution, submitInProgress } = this.state
     return (
       <div className='HomeworkSubmission'>
         {!open ? (
@@ -22,13 +129,34 @@ export default class HomeworkSubmission extends React.Component {
             basic
             size='tiny'
             color='blue'
-            content='Odevzdat'
+            content='Odevzdat řešení'
             icon='reply'
             onClick={this.handleOpenButton}
           />
         ) : (
-          <div style={{ animation: 'slide-up 0.4s ease' }}>
-            <HomeworkSubmitForm />
+          <div style={{ animation: 'slide-up 0.4s ease', marginTop: '1.5rem' }}>
+            {(!currentSolution || editCurrentSolution) ? (
+              <HomeworkSolutionForm
+                onSubmit={this.handleSubmitSolution}
+                code={currentSolution ? currentSolution.code : null}
+                loading={submitInProgress}
+              />
+            ) : (
+              <>
+                <HomeworkSolution code={currentSolution.code} />
+                <Button
+                  basic
+                  size='tiny'
+                  color='blue'
+                  content='Upravit'
+                  icon='edit'
+                  onClick={this.handleEditButton}
+                />
+                <HomeworkComments />
+              </>
+            )}
+
+
           </div>
         )}
         <style jsx>{`
