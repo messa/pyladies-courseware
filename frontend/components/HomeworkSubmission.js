fccd2fb771384b7deac193b86fe5cdac3a9ba050
Petr Messner <petr.messner@gmail.com>
2018-07-29 11:40:38 +0200

    wip homework submission - comments UI

--- frontend/components/HomeworkSubmission.js
+++ frontend/components/HomeworkSubmission.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Button } from 'semantic-ui-react'
-import { Comment, Form, Header } from 'semantic-ui-react'
+import { Comment, Form, Header, Icon, TextArea } from 'semantic-ui-react'
 import HomeworkSolutionForm from './HomeworkSolutionForm'
 
 const HomeworkSolution = ({ code }) => (
@@ -10,34 +10,15 @@ const HomeworkSolution = ({ code }) => (
   </div>
 )
 
-class CommentReply extends React.Component {
+class CommentContent extends React.Component {
 
-  render() {
-    return (
-      <Comment>
-        <Comment.Avatar src='https://react.semantic-ui.com/images/avatar/small/jenny.jpg' />
-        <Comment.Content>
-          <Comment.Author as='a'>Jenny Hess</Comment.Author>
-          <Comment.Metadata>
-            <div>Just now</div>
-          </Comment.Metadata>
-          <Comment.Text>Elliot you are always so right :)</Comment.Text>
-          <Comment.Actions>
-            <Comment.Action>Reply</Comment.Action>
-          </Comment.Actions>
-        </Comment.Content>
-      </Comment>
-    )
+  handleReplyAction = () => {
+    this.props.onReply()
   }
 
-}
-
-
-class CommentThread extends React.Component {
-
   render() {
     return (
-      <Comment>
+      <>
         <Comment.Avatar src='https://react.semantic-ui.com/images/avatar/small/elliot.jpg' />
         <Comment.Content>
           <Comment.Author as='a'>Elliot Fu</Comment.Author>
@@ -48,13 +29,85 @@ class CommentThread extends React.Component {
             <p>This has been very useful for my research. Thanks as well!</p>
           </Comment.Text>
           <Comment.Actions>
-            <Comment.Action>Reply</Comment.Action>
+            <Comment.Action onClick={this.handleReplyAction}>
+              <Icon name='reply' /> Odpovědět
+            </Comment.Action>
           </Comment.Actions>
         </Comment.Content>
+      </>
+    )
+  }
+}
+
+class CommentReply extends React.Component {
+
+  handleReplyAction = () => {
+    this.props.onReply()
+  }
+
+  render() {
+    return (
+      <Comment>
+        <CommentContent onReply={this.handleReplyAction} />
+      </Comment>
+    )
+  }
+
+}
+
+
+class CommentThread extends React.Component {
+
+  state = {
+    showReplyForm: false,
+  }
+
+  textAreaRef = null
+
+  setTextAreaRef = (c) => {
+    this.textAreaRef = c
+  }
+
+  handleReplyAction = () => {
+    this.setState({ showReplyForm: true }, () => {
+      try {
+        this.textAreaRef.focus()
+      } catch (err) {
+        console.error(`TextArea focus failed: ${err}`)
+      }
+    })
+
+  }
+
+  handleCancel = () => {
+    this.setState({ showReplyForm: false })
+  }
+
+  render() {
+    const { showReplyForm } = this.state
+    return (
+      <Comment>
+        <CommentContent onReply={this.handleReplyAction} />
+
         <Comment.Group>
-          <CommentReply />
-          <CommentReply />
+          <CommentReply onReply={this.handleReplyAction} />
+          <CommentReply onReply={this.handleReplyAction} />
         </Comment.Group>
+
+        {showReplyForm && (
+          <Comment.Content style={{ animation: 'slide-up 0.4s ease' }}>
+            <Form reply>
+              <Form.Field>
+                <TextArea ref={this.setTextAreaRef} />
+              </Form.Field>
+              <Button content='Odeslat odpověď' icon='send' primary />
+              &nbsp;{' '}
+              <Button content='Zrušit' icon='cancel' color='red' basic onClick={this.handleCancel} />
+            </Form>
+          </Comment.Content>
+        )}
+
+
       </Comment>
 
     )
@@ -64,23 +117,12 @@ class CommentThread extends React.Component {
 
 class HomeworkComments extends React.Component {
 
-  state = {
-    replyTo: null,
-  }
-
   render() {
-    const { replyTo } = this.state
     return (
       <div style={{ marginTop: '1.5rem' }}>
         <Comment.Group>
           <CommentThread />
 
-          {replyTo !== null && (
-            <Form reply>
-              <Form.TextArea />
-              <Button content='Add Reply' size='small' icon='edit' primary />
-            </Form>
-          )}
         </Comment.Group>
       </div>
     )
