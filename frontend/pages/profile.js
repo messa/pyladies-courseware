d1099210a028b55138693fbd575174c35f496f64
Petr Messner <petr.messner@gmail.com>
2018-08-30 22:31:13 +0200

    frontend: profile page

--- frontend/pages/profile.js
+++ frontend/pages/profile.js
@@ -15,7 +15,42 @@ export default class ProfilePage extends React.Component {
     return (
       <Layout user={user}>
 
-        <pre>{JSON.stringify(user, null, 2)}</pre>
+        <h1>Tvůj profil</h1>
+
+        <p>Jméno: {user.name}</p>
+        <p>id: <code>{user.id}</code></p>
+
+        {user.attended_course_ids.length > 0 && (
+          <>
+            <p>Navštěvované kurzy:</p>
+            <ul>
+              {user.attended_course_ids.map(course_id => (
+                <li key={course_id}>
+                  {course_id}
+                </li>
+              ))}
+            </ul>
+          </>
+        )}
+
+        {user.coached_course_ids.length > 0 && (
+          <>
+            <p>Koučované kurzy:</p>
+            <ul>
+              {user.coached_course_ids.map(course_id => (
+                <li key={course_id}>
+                  {course_id}
+                </li>
+              ))}
+            </ul>
+          </>
+        )}
+
+        {user.is_admin && (
+          <p>Admin</p>
+        )}
+
+        <pre className='debug' style={{ display: 'none' }}>{JSON.stringify(user, null, 2)}</pre>
 
       </Layout>
     )
