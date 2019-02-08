d06c5af12a02e2f7727fecdaa34af1d074075e9a
Petr Messner <petr.messner@gmail.com>
2019-02-08 12:19:05 +0100

    WIP

--- backend/cw_backend/graphql/schema.py
+++ backend/cw_backend/graphql/schema.py
@@ -1,8 +1,5 @@
-from asyncio import create_task, gather, Event, CancelledError
-from collections import defaultdict, namedtuple
-from functools import wraps
-from inspect import iscoroutinefunction
 import logging
+
 from graphql import (
     graphql,
     GraphQLSchema,
@@ -19,37 +16,34 @@ from graphql import (
     GraphQLBoolean,
 )
 
-from .relay_helpers import connection_args, connection_from_list, relay_connection_type, mutation
+from .relay_helpers import (
+    connection_args,
+    connection_from_list,
+    relay_connection_type,
+    mutation,
+)
 
 
 logger = logging.getLogger(__name__)
 
 
-conversation_update_events = defaultdict(list) # { topic id: [Event] }
+# conversation_update_events = defaultdict(list) # { topic id: [Event] }
 
 
-def with_model(f):
-    '''
-    Decorator for passing model to resolver functions
-    '''
-    assert iscoroutinefunction(f)
-    async def wrapper(parent, info, *args, **kwargs):
-        assert 'model' not in kwargs
-        model = info.context['request'].app['model']
-        return await f(parent, info, model, *args, **kwargs)
-    return wrapper
+def get_model(info):
+    return info.context['request'].app['model']
 
 
 def resolve_node_type(obj, info):
     # See also: https://stackoverflow.com/q/34726666/196206
-    if obj.node_type == 'Post':
-        return Post
-    if obj.node_type == 'Category':
-        return Category
-    if obj.node_type == 'Topic':
-        return Topic
-    if obj.node_type == 'Conversation':
-        return Conversation
+    # if obj.node_type == 'Post':
+    #     return Post
+    # if obj.node_type == 'Category':
+    #     return Category
+    # if obj.node_type == 'Topic':
+    #     return Topic
+    # if obj.node_type == 'Conversation':
+    #     return Conversation
     raise Exception(f'Unknown node type: {obj!r}')
 
 
@@ -61,182 +55,183 @@ NodeInterface = GraphQLInterfaceType(
     resolve_type=resolve_node_type)
 
 
-ReplyPost = GraphQLObjectType(
-    name='ReplyPost',
-    interfaces=[NodeInterface],
-    fields=lambda: {
-        'id': GraphQLField(type=GraphQLNonNull(GraphQLID)),
-        'bodyMarkdown': GraphQLField(
-            type=GraphQLString,
-            resolver=lambda post, info: post.body_markdown),
-    })
+# ReplyPost = GraphQLObjectType(
+#     name='ReplyPost',
+#     interfaces=[NodeInterface],
+#     fields=lambda: {
+#         'id': GraphQLField(type=GraphQLNonNull(GraphQLID)),
+#         'bodyMarkdown': GraphQLField(
+#             type=GraphQLString,
+#             resolver=lambda post, info: post.body_markdown),
+#     })
+#
+#
+# ReplyPostConnection = relay_connection_type(ReplyPost)
+#
+# @with_model
+# async def post_replies_resolver(post, info, model, **kwargs):
+#     replies = await model.list_post_replies(post.id)
+#     return connection_from_list(replies, **kwargs)
+#
+#
+# Post = GraphQLObjectType(
+#     name='Post',
+#     interfaces=[NodeInterface],
+#     fields=lambda: {
+#         'id': GraphQLField(type=GraphQLNonNull(GraphQLID)),
+#         'bodyMarkdown': GraphQLField(
+#             type=GraphQLString,
+#             resolver=lambda post, info: post.body_markdown),
+#         'replies': GraphQLField(
+#             type=ReplyPostConnection,
+#             args=connection_args,
+#             resolver=post_replies_resolver),
+#     })
+#
+#
+# PostConnection = relay_connection_type(Post)
+#
+#
+# @with_model
+# async def conversation_posts_resolver(conversation, info, model, **kwargs):
+#     logger.debug('conversation_posts_resolver(%r)', conversation)
+#     posts = await model.list_conversation_posts(conversation_id=conversation.id)
+#     return connection_from_list(posts, **kwargs)
+#
+#
+# Conversation = GraphQLObjectType(
+#     name='Conversation',
+#     interfaces=[NodeInterface],
+#     fields=lambda: {
+#         'id': GraphQLField(type=GraphQLNonNull(GraphQLID)),
+#         'posts': GraphQLField(
+#             type=PostConnection,
+#             args=connection_args,
+#             resolver=conversation_posts_resolver),
+#     })
+#
+#
+# ConversationConnection = relay_connection_type(Conversation)
+#
+#
+# @with_model
+# async def topic_conversation_resolver(topic, info, model):
+#     return await model.get_conversation(topic.conversation_id)
+#
+#
+# @with_model
+# async def topic_category_resolver(topic, info, model):
+#     categories = await model.list_categories()
+#     categories = [cat for cat in categories if topic.id in cat.topic_ids]
+#     category, = categories
+#     return category
+#
+#
+# Topic = GraphQLObjectType(
+#     name='Topic',
+#     interfaces=[NodeInterface],
+#     fields=lambda: {
+#         'id': GraphQLField(type=GraphQLNonNull(GraphQLID)),
+#         'title': GraphQLField(type=GraphQLString),
+#         'conversation': GraphQLField(
+#             type=Conversation,
+#             resolver=topic_conversation_resolver),
+#         'category': GraphQLField(
+#             type=Category,
+#             resolver=topic_category_resolver),
+#     })
+#
+#
+# TopicConnection = relay_connection_type(Topic)
+#
+#
+# @with_model
+# async def category_topics_resolver(category, info, model, **kwargs):
+#     return connection_from_list(await gather(*(
+#         model.get_topic(topic_id) for topic_id in category.topic_ids)),
+#         **kwargs)
+#
+#
+# Category = GraphQLObjectType(
+#     name='Category',
+#     interfaces=[NodeInterface],
+#     fields={
+#         'id': GraphQLField(type=GraphQLNonNull(GraphQLID)),
+#         'title': GraphQLField(type=GraphQLString),
+#         'topics': GraphQLField(
+#             type=TopicConnection,
+#             resolver=category_topics_resolver),
+#     })
+#
+#
+# CategoryConnection = relay_connection_type(Category)
 
 
-ReplyPostConnection = relay_connection_type(ReplyPost)
-
-@with_model
-async def post_replies_resolver(post, info, model, **kwargs):
-    replies = await model.list_post_replies(post.id)
-    return connection_from_list(replies, **kwargs)
-
-
-Post = GraphQLObjectType(
-    name='Post',
-    interfaces=[NodeInterface],
-    fields=lambda: {
-        'id': GraphQLField(type=GraphQLNonNull(GraphQLID)),
-        'bodyMarkdown': GraphQLField(
-            type=GraphQLString,
-            resolver=lambda post, info: post.body_markdown),
-        'replies': GraphQLField(
-            type=ReplyPostConnection,
-            args=connection_args,
-            resolver=post_replies_resolver),
-    })
+async def node_resolver(root, info, id):
+    model = get_model(info)
+    raise Exception('NIY')
+    #obj = await model.get_by_id(id)
+    #logger.debug('node_resolver %r -> %r', id, obj)
+    #return obj
 
 
-PostConnection = relay_connection_type(Post)
-
-
-@with_model
-async def conversation_posts_resolver(conversation, info, model, **kwargs):
-    logger.debug('conversation_posts_resolver(%r)', conversation)
-    posts = await model.list_conversation_posts(conversation_id=conversation.id)
-    return connection_from_list(posts, **kwargs)
-
-
-Conversation = GraphQLObjectType(
-    name='Conversation',
-    interfaces=[NodeInterface],
-    fields=lambda: {
-        'id': GraphQLField(type=GraphQLNonNull(GraphQLID)),
-        'posts': GraphQLField(
-            type=PostConnection,
-            args=connection_args,
-            resolver=conversation_posts_resolver),
-    })
-
-
-ConversationConnection = relay_connection_type(Conversation)
-
-
-@with_model
-async def topic_conversation_resolver(topic, info, model):
-    return await model.get_conversation(topic.conversation_id)
-
-
-@with_model
-async def topic_category_resolver(topic, info, model):
-    categories = await model.list_categories()
-    categories = [cat for cat in categories if topic.id in cat.topic_ids]
-    category, = categories
-    return category
-
-
-Topic = GraphQLObjectType(
-    name='Topic',
-    interfaces=[NodeInterface],
-    fields=lambda: {
-        'id': GraphQLField(type=GraphQLNonNull(GraphQLID)),
-        'title': GraphQLField(type=GraphQLString),
-        'conversation': GraphQLField(
-            type=Conversation,
-            resolver=topic_conversation_resolver),
-        'category': GraphQLField(
-            type=Category,
-            resolver=topic_category_resolver),
-    })
-
-
-TopicConnection = relay_connection_type(Topic)
-
-
-@with_model
-async def category_topics_resolver(category, info, model, **kwargs):
-    return connection_from_list(await gather(*(
-        model.get_topic(topic_id) for topic_id in category.topic_ids)),
-        **kwargs)
-
-
-Category = GraphQLObjectType(
-    name='Category',
-    interfaces=[NodeInterface],
-    fields={
-        'id': GraphQLField(type=GraphQLNonNull(GraphQLID)),
-        'title': GraphQLField(type=GraphQLString),
-        'topics': GraphQLField(
-            type=TopicConnection,
-            resolver=category_topics_resolver),
-    })
-
-
-CategoryConnection = relay_connection_type(Category)
-
-
-@with_model
-async def node_resolver(root, info, model, id):
-    obj = await model.get_by_id(id)
-    logger.debug('node_resolver %r -> %r', id, obj)
-    return obj
-
-
-@with_model
-async def categories_resolver(root, info, model, **kwargs):
-    return connection_from_list(await model.list_categories(), **kwargs)
-
-
-async def handle_post_reply(info, **kwargs):
-    logger.info('handle_post_reply(%r, **%r)', info, kwargs)
-    model = info.context['request'].app['model']
-    reply_post = await model.create_reply_post(kwargs['postId'], kwargs['bodyMarkdown'])
-    for event in conversation_update_events[reply_post.conversation_id]:
-        logger.info('Notifying event %r for conversation %r', event, reply_post.conversation_id)
-        event.set()
-    return reply_post
-
-
-PostReplyMutation = mutation(
-    name='PostReply',
-    input_fields={
-        'postId': GraphQLInputObjectField(type=GraphQLNonNull(GraphQLID)),
-        'bodyMarkdown': GraphQLInputObjectField(type=GraphQLNonNull(GraphQLString)),
-    },
-    output_fields={
-        'newReplyPost': GraphQLField(type=ReplyPost),
-    },
-    mutate_and_get_payload=handle_post_reply)
-
-
-async def resolve_count_seconds(root, info):
-    from asyncio import sleep
-    for i in range(100):
-        yield i
-        await sleep(1)
-
-
-async def resolve_conversation_updated(root, info, *, conversationId):
-    try:
-        model = info.context['request'].app['model']
-        event = Event()
-        conversation_update_events[conversationId].append(event)
-        logger.info('Placed event %r for conversation %r', event, conversationId)
-        try:
-            while True:
-                await event.wait()
-                logger.info('Triggered event %r for conversation %r', event, conversationId)
-                event.clear()
-                conversation = await model.get_by_id(conversationId)
-                logger.info('Yielding conversation %r', conversation)
-                yield conversation
-        finally:
-            conversation_update_events[conversationId].remove(event)
-            logger.info('Removed event %r for conversation %r', event, conversationId)
-    except CancelledError:
-        logger.info('resolve_conversation_updated %r', e)
-        return
-    except Exception as e:
-        logger.exception('resolve_conversation_updated failed: %r', e)
-        raise e
+# @with_model
+# async def categories_resolver(root, info, model, **kwargs):
+#     return connection_from_list(await model.list_categories(), **kwargs)
+#
+#
+# async def handle_post_reply(info, **kwargs):
+#     logger.info('handle_post_reply(%r, **%r)', info, kwargs)
+#     model = info.context['request'].app['model']
+#     reply_post = await model.create_reply_post(kwargs['postId'], kwargs['bodyMarkdown'])
+#     for event in conversation_update_events[reply_post.conversation_id]:
+#         logger.info('Notifying event %r for conversation %r', event, reply_post.conversation_id)
+#         event.set()
+#     return reply_post
+#
+#
+# PostReplyMutation = mutation(
+#     name='PostReply',
+#     input_fields={
+#         'postId': GraphQLInputObjectField(type=GraphQLNonNull(GraphQLID)),
+#         'bodyMarkdown': GraphQLInputObjectField(type=GraphQLNonNull(GraphQLString)),
+#     },
+#     output_fields={
+#         'newReplyPost': GraphQLField(type=ReplyPost),
+#     },
+#     mutate_and_get_payload=handle_post_reply)
+#
+#
+# async def resolve_count_seconds(root, info):
+#     from asyncio import sleep
+#     for i in range(100):
+#         yield i
+#         await sleep(1)
+#
+#
+# async def resolve_conversation_updated(root, info, *, conversationId):
+#     try:
+#         model = info.context['request'].app['model']
+#         event = Event()
+#         conversation_update_events[conversationId].append(event)
+#         logger.info('Placed event %r for conversation %r', event, conversationId)
+#         try:
+#             while True:
+#                 await event.wait()
+#                 logger.info('Triggered event %r for conversation %r', event, conversationId)
+#                 event.clear()
+#                 conversation = await model.get_by_id(conversationId)
+#                 logger.info('Yielding conversation %r', conversation)
+#                 yield conversation
+#         finally:
+#             conversation_update_events[conversationId].remove(event)
+#             logger.info('Removed event %r for conversation %r', event, conversationId)
+#     except CancelledError:
+#         logger.info('resolve_conversation_updated %r', e)
+#         return
+#     except Exception as e:
+#         logger.exception('resolve_conversation_updated failed: %r', e)
+#         raise e
 
 
 Schema = GraphQLSchema(
@@ -249,29 +244,30 @@ Schema = GraphQLSchema(
                     'id': GraphQLArgument(GraphQLNonNull(GraphQLID)),
                 },
                 resolver=node_resolver),
-            'categories': GraphQLField(
-                type=CategoryConnection,
-                args=connection_args,
-                resolver=categories_resolver),
+            # 'courses': GraphQLField(
+            #     type=CourseConnection,
+            #     args=connection_args,
+            #     resolver=courses_resolver),
         }
     ),
-    mutation=GraphQLObjectType(
-        name='Mutation',
-        fields={
-            'postReply': PostReplyMutation,
-        }
-    ),
-    subscription=GraphQLObjectType(
-        name='Subscription',
-        fields={
-            'countSeconds': GraphQLField(
-                type=GraphQLInt,
-                resolver=resolve_count_seconds),
-            'conversationUpdated': GraphQLField(
-                type=Conversation,
-                args={
-                    'conversationId': GraphQLArgument(GraphQLNonNull(GraphQLID)),
-                },
-                resolver=resolve_conversation_updated),
-        }
-    ))
+    # mutation=GraphQLObjectType(
+    #     name='Mutation',
+    #     fields={
+    #         'postReply': PostReplyMutation,
+    #     }
+    # ),
+    # subscription=GraphQLObjectType(
+    #     name='Subscription',
+    #     fields={
+    #         'countSeconds': GraphQLField(
+    #             type=GraphQLInt,
+    #             resolver=resolve_count_seconds),
+    #         'conversationUpdated': GraphQLField(
+    #             type=Conversation,
+    #             args={
+    #                 'conversationId': GraphQLArgument(GraphQLNonNull(GraphQLID)),
+    #             },
+    #             resolver=resolve_conversation_updated),
+    #     }
+    # )
+)
--- backend/cw_backend/main.py
+++ backend/cw_backend/main.py
@@ -4,11 +4,15 @@ from aiohttp_session_mongo import MongoStorage
 import argparse
 import logging
 from motor.motor_asyncio import AsyncIOMotorClient
+from aiohttp_graphql import GraphQLView
+from graphql.execution.executors.asyncio import AsyncioExecutor as GQLAIOExecutor
+from graphql_ws.aiohttp import AiohttpSubscriptionServer
 
 from .configuration import Configuration
 from .courses import load_courses
 from .model import Model
 from .views import all_routes
+from .graphql import Schema
 
 
 logger = logging.getLogger(__name__)
@@ -30,6 +34,7 @@ async def get_app(conf):
     mongo_db = mongo_client[conf.mongodb.db_name]
     model = Model(mongo_db, conf)
     await model.create_indexes()
+
     app = web.Application()
 
     async def close_mongo(app):
@@ -38,11 +43,36 @@ async def get_app(conf):
     app.on_cleanup.append(close_mongo)
 
     session_setup(app, MongoStorage(mongo_db['sessions'], max_age=3600*24*90))
+
     app['conf'] = conf
     app['courses'] = load_courses(conf.courses_file)
     app['model'] = model
+
     app.add_routes(all_routes)
+
+    GraphQLView.attach(
+        app,
+        route_path='/api/graphql',
+        schema=Schema,
+        graphiql=True,
+        enable_async=True,
+        executor=GQLAIOExecutor())
+
+    app.router.add_get('/api/subscriptions', subscriptions)
+
     return app
+
+
+async def subscriptions(request):
+    try:
+        ws = web.WebSocketResponse(protocols=('graphql-ws',))
+        await ws.prepare(request)
+        subscription_server = AiohttpSubscriptionServer(Schema)
+        await subscription_server.handle(ws, {'request': request})
+        return ws
+    except Exception as e:
+        logger.exception('subscriptions failed: %r', e)
+        raise e
 
 
 def setup_logging():
