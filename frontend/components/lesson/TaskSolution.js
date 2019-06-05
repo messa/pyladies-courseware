4e7512e52111dce620aad6d6fe35137ac8284d0d
Miroslav Brabenec <brabemi@gmail.com>
2019-06-05 16:07:33 +0200

    Solution versions in task solution view

--- frontend/components/lesson/TaskSolution.js
+++ frontend/components/lesson/TaskSolution.js
@@ -1,17 +1,34 @@
 import React from 'react'
+import { Label, Tab } from 'semantic-ui-react'
 
 export default ({ taskSolution }) => {
   if (!taskSolution) return null
   const currentVersion = taskSolution.current_version
   if (!currentVersion) return null
+  const allVersions = taskSolution.all_versions
+  if (!allVersions) return null
+  const sortedVersions = allVersions.slice().sort(function (a, b) { return b.date.localeCompare(a.date) })
+  var cntr = sortedVersions.length
+  const panes = sortedVersions.slice(0, 8).map(
+    function (v) {
+      name = 'Verze ' + cntr
+      cntr -= 1
+      return {
+        menuItem: name,
+        render: () => <Tab.Pane>
+          <div className='TaskSolution'>
+            <Label attached='bottom right'>{v.date}</Label>
+            <pre>{v.code}</pre>
+            <style jsx>{`
+              .TaskSolution pre {
+                overflow-x: auto;
+              }
+            `}</style>
+          </div>
+        </Tab.Pane>
+      }
+    })
   return (
-    <div className='TaskSolution'>
-      <pre>{currentVersion.code}</pre>
-      <style jsx>{`
-        .TaskSolution pre {
-          overflow-x: auto;
-        }
-      `}</style>
-    </div>
+    <Tab panes={panes} />
   )
 }
