94c91888e108e398a982ab272a1531951f82aeed
Miroslav Brabenec <brabemi@gmail.com>
2019-09-28 20:25:12 +0200

    Add copy to clipboard button to task solution component

--- frontend/components/TaskSubmission.js
+++ frontend/components/TaskSubmission.js
@@ -2,43 +2,9 @@ import React from 'react'
 import { Button, Label, Tab } from 'semantic-ui-react'
 import { Comment, Form, Header, Icon, TextArea, Message } from 'semantic-ui-react'
 import TaskSolutionForm from './TaskSolutionForm'
+import TaskSolution from './lesson/TaskSolution'
 import TaskComments from './lesson/TaskComments'
 import holdAnchor from './Helpers'
-
-const TaskSolution = ({ taskSolution }) => {
-  if (!taskSolution) return null
-  const currentVersion = taskSolution.current_version
-  if (!currentVersion) return null
-  const allVersions = taskSolution.all_versions
-  if (!allVersions) return null
-  const sortedVersions = allVersions.slice().sort(function (a, b) { return b.date.localeCompare(a.date) })
-  var cntr = sortedVersions.length
-  const panes = sortedVersions.slice(0, 8).map(
-    function (v) {
-      name = 'Verze ' + cntr
-      cntr -= 1
-      return {
-        menuItem: name,
-        render: () => <Tab.Pane>
-          <div className='TaskSolution'>
-            <Label attached='bottom right'>{v.date}</Label>
-            <pre>{v.code}</pre>
-            <style jsx>{`
-              .TaskSolution pre {
-                overflow-x: auto;
-              }
-            `}</style>
-          </div>
-        </Tab.Pane>
-      }
-    })
-  return (
-    <div>
-      <h4>Odevzdané řešení <TaskStatus taskSolution={taskSolution} /></h4>
-      <Tab panes={panes}/>
-    </div>
-  )
-}
 
 export default class TaskSubmission extends React.Component {
 
@@ -206,7 +172,10 @@ export default class TaskSubmission extends React.Component {
     } else if (taskSolution) {
       content = (
         <>
-          <TaskSolution taskSolution={taskSolution} />
+          <div>
+            <h4>Odevzdané řešení <TaskStatus taskSolution={taskSolution} /></h4>
+            <TaskSolution taskSolution={taskSolution} />
+          </div>
           {taskSolution.is_solved ? (
             <div>
               <Icon
