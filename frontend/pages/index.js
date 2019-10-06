78ebf96c992260c986c2adca699bb32fb7a1e72d
Miroslav Brabenec <brabemi@gmail.com>
2019-10-06 16:02:35 +0200

    Added my courses section to index page

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -11,9 +11,27 @@ export default class extends React.Component {
 
   render() {
     const { user, courses } = this.props
+    const my_courses_ids = user ? user.attended_course_ids.concat(user.coached_course_ids) : []
+    const active_courses_ids = new Set(courses.active.map(c => c.id))
+    const my_active_courses_ids = new Set(my_courses_ids.filter(cid => active_courses_ids.has(cid)))
     return (
       <Layout user={user} width={600}>
 
+        { my_active_courses_ids.size > 0 && (
+          <>
+            <h2>Moje kurzy</h2>
+
+            {courses.active.filter(c => my_active_courses_ids.has(c.id)).map(course => (
+              <p key={course.id} className='course'>
+                <Link href={{ pathname: '/course', query: { course: course.id }}} prefetch><a>
+                  <strong dangerouslySetInnerHTML={{__html: course.title_html}} />
+                  {' – '}
+                  <span dangerouslySetInnerHTML={{__html: course.subtitle_html}} />
+                </a></Link>
+              </p>
+            ))}
+          </>
+        )}
         <h2>Aktuálně běžící kurzy</h2>
 
         {courses.active.map(course => (
