ec82f98253dd9e42086368e7aa1121808c812af2
Václav Fiala <60704196+OpposedOak@users.noreply.github.com>
2022-01-09 13:23:22 +0100

    Add files via upload

---
+++ data/2022_pyladies_plzen_spring/Templates/Had_template.py
@@ -0,0 +1,44 @@
+def vytvor_tabulku(vyska,sirka):
+    "Funkce vytvoří tabulku podle zadané výšk a šířky"
+    #Vstupní tabulka
+    #Naplěnní tabulky
+    pass
+
+def nakresli_mapu(souradnice,seznam_ovoce,kolo):
+    "Funkce přepíše prvek podle zvolených souřadnic a vykreslí tabulku"
+    #Vytvoření tabulky
+    #Přepsání prvku "." za "x"
+    #Přepsání prvku "." za "o"
+    #Vytvoreni ovoce
+    #Vytvoření ovoe ve 30 kole   
+    #Vykreslení tabulky
+    pass
+
+def pohyb(souradnice,smer,seznam_ovoce):
+    "Funkce má za úkol přiřazení souřadnic"
+    #Tah na východ
+    #Tah na západ
+    #Tah na jih   
+    #Tah na sever   
+    #Ochrana proti nabourani do těla
+    #Ochrana proti narazu do zdi sever, zapad   
+    #Ochrana proti narazu do zdi jih východ 
+    #Kontrola kontaktu s ovocem
+    #Zachování delky při kontaktu s ovocem
+    pass
+        
+def ovoce(tabulka):
+    "Funkce vrátí souřadnice pro umístěné ovoce"
+    # Ověření obsazenosti pole a případně nalezení volného pole. 
+    pass
+
+def had():
+    "Funkce, která řídí průběh hry"
+    #Výchozí souřadnice
+    #Rozměry pole
+    #Počítadlo kol
+    #Souradnice ovoce
+    #Herní smyčka
+    pass
+
+had()
